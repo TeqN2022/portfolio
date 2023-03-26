dbfe38922cd074189abfd2def3712f00200715b7
TeqN <info@teqn.xyz>
2023-03-27 00:35:30 +0300

    v1

---
+++ README.md
@@ -0,0 +1,33 @@
+# Portfolio made by [Kalashiexe](https://github.com/Kalashiexe)
+
+## 📝 Credits
+
+ - [Next.js](https://nextjs.org/)
+ - [Tailwind CSS](https://tailwindcss.com/)
+
+ Description: This is my portfolio website. I made it with Next.js and Tailwind CSS. I used Next.js for the server side rendering and Tailwind CSS for the styling. I used the Next.js with Tailwind CSS
+## 🚀 Demo
+
+https://kalashi.me
+
+## 📦 Installation
+
+```bash 
+git clone https://github.com/Kalashiexe/Portfolio.git
+```
+
+## 🛠 Setup
+
+```bash
+cd Portfolio
+npm install --s --f && npm run dev
+```
+
+## 📄 License
+
+MIT © [Kalashiexe](./LICENSE)
+
+## 📫 Contact
+
+[Gmail](mailto:kalashispotify@gmail.com) |  [Discord](https://discord.com/users/1059362574958874634) | [Github](https://github.com/Kalashiexe)
+
---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    discord:{
+        id: "954355224305356840", 
+    },
+    github:{
+        token1: "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"
+    },
+    about:{
+        description: "I'm a 16 years old developer from Turkey. I'm currently working on my own projects and learning new things.",
+    },
+    social:{
+        github: "https://github.com/TeqN2022",
+        instagram: "https://instagram.com/teqnexe",
+    },
+    meta:{
+        title: 'My Personal Website',
+        description: 'My Personal Website',
+        theme: '#02060f',
+        lang: 'tr',
+        url: 'https://teqn.xyz',
+        image: '',
+    },
+    title: 'TeqN',
+    titles:[
+        {title: 'Home', url: '/', icon: 'fa-house'},
+        {title: 'Projects', url: '/projects' ,icon: 'fa-blog'},
+        {title: 'Blog', url: '/blog' , icon: 'fa-code'},
+    ]
+}
+
