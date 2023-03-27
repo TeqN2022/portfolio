90024a12f13c415222a878d7c40d658326312d67
TeqN <118697424+TeqN2022@users.noreply.github.com>
2023-03-27 19:02:35 +0300

    Update project.jsx

--- src/pages/api/v1/project.jsx
+++ src/pages/api/v1/project.jsx
@@ -1,17 +1,23 @@
 export default async(req, res) => { 
     const project = [
         {
-            "name": "Kalashi.me",
-            "link": "https://i.imgur.com/x7J5oG0.png",
+            "name": "teqn.xyz",
+            "link": "https://cdn.discordapp.com/attachments/1075180794471010315/1089677960132436118/image.png",
             "description": "A personal website built with Next.js and Tailwind CSS",
-            "html_url": "https://github.com/Kalashiexe/Kalashi.me"
+            "html_url": "https://teqn.xyz/"
         
         },
         {
-            "name": "7bin.co",
-            "link": "https://cdn.discordapp.com/attachments/1061295779853123684/1064914867909296188/image.png",
-            "description": "A pastebin clone built with Next.js and Tailwind CSS",
-            "html_url": "https://7bin.co"
+            "name": "tr2a.xyz",
+            "link": "https://cdn.discordapp.com/attachments/1075180794471010315/1089678842844676209/image.png",
+            "description": "",
+            "html_url": "https://tr2a.xyz/"
+        },
+        {
+            "name": "mybomb.store",
+            "link": "https://cdn.discordapp.com/attachments/1075180794471010315/1089679104204361778/image.png",
+            "description": "",
+            "html_url": "https://mybomb.store/"
         },
 
         
